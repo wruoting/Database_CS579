8e3b36916ac312fb5563d445dea8f966f1d60730
ruotingw <wruoting@gmail.com>
2019-06-19 23:17:04 -0400

    insert statements

--- parse_json.py
+++ parse_json.py
@@ -23,28 +23,41 @@ with open("AllCards.json", encoding='utf-8') as file:
     toughness_header = []
 
     # prices_header = []
-    for name in json_data:
-        card_name_header.append(name)
-        card_data = json_data[name]
-        uuid_header.append(card_data['uuid']) if card_data.get('uuid') else uuid_header.append('')
-        color_header.append(card_data['colors']) if card_data.get('colors') else color_header.append('')
-        loyalty_header.append(card_data['loyalty']) if card_data.get('loyalty') else loyalty_header.append('')
-        mana_cost_header.append(card_data['manaCost']) if card_data.get('manaCost') else mana_cost_header.append('')
-        original_text_header.append(card_data['text']) if card_data.get('text') else original_text_header.append('')
-        original_type_header.append(card_data['originalType']) if card_data.get('originalType') else original_type_header.append('')
-        power_header.append(card_data['power']) if card_data.get('power') else power_header.append('')
-        toughness_header.append(card_data['toughness']) if card_data.get('toughness') else toughness_header.append('')
-    data = {
-        'name' : card_name_header,
-        'uuid' : uuid_header,
-        'color': color_header,
-        'loyalty': loyalty_header,
-        'mana_cost': mana_cost_header,
-        'text': original_text_header,
-        'type': original_type_header,
-        'power': power_header,
-        'toughness': toughness_header
-    }
+    with open("insert_card_query.sql", 'wb') as file_2:
+        for name in json_data:
+            card_name_header.append(name)
+            card_data = json_data[name]
+            uuid_header.append(card_data['uuid']) if card_data.get('uuid') else uuid_header.append('')
+            color_header.append(card_data['colors']) if card_data.get('colors') else color_header.append('')
+            loyalty_header.append(card_data['loyalty']) if card_data.get('loyalty') else loyalty_header.append('')
+            mana_cost_header.append(card_data['manaCost']) if card_data.get('manaCost') else mana_cost_header.append('')
+            original_text_header.append(card_data['text']) if card_data.get('text') else original_text_header.append('')
+            original_type_header.append(card_data['originalType']) if card_data.get('originalType') else original_type_header.append('')
+            power_header.append(card_data['power']) if card_data.get('power') else power_header.append('')
+            toughness_header.append(card_data['toughness']) if card_data.get('toughness') else toughness_header.append('')
+            uuid = card_data['uuid'] if card_data.get('uuid') else ''
+            colors = card_data['colors'] if card_data.get('colors') else ''
+            loyalty = card_data['loyalty'] if card_data.get('loyalty') else ''
+            manaCost = card_data['manaCost'] if card_data.get('manaCost') else ''
+            text = card_data['text'] if card_data.get('text') else ''
+            originalType = card_data['originalType'] if card_data.get('originalType') else ''
+            power = card_data['power'] if card_data.get('power') else ''
+            toughness = card_data['toughness'] if card_data.get('toughness') else ''
+            str = "INSERT INTO CARDS (uuid, name, color, loyalty, mana_cost, text, type, power, toughness) VALUES ({},{},{},{},{},{},{},{},{});\n".format(uuid, name, colors, loyalty, manaCost, text, originalType, power, toughness)
+            file_2.write(str.encode('utf-8'))
+        data = {
+            'name' : card_name_header,
+            'uuid' : uuid_header,
+            'color': color_header,
+            'loyalty': loyalty_header,
+            'mana_cost': mana_cost_header,
+            'text': original_text_header,
+            'type': original_type_header,
+            'power': power_header,
+            'toughness': toughness_header
+        }
+
+        
     s = pd.DataFrame(data)
     s.to_csv('Card_Table.csv', sep='\t', index=False, encoding='utf-8')
     
